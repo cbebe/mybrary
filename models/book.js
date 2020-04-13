1993f8805c187ed63c6108ba85e17df4cf572f9f
charlesancheta <cancheta@ualberta.ca>
2020-04-13 00:46:30 -0600

    kek

---
+++ models/book.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImageName: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
