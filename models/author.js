e6c040d585b08a2a309c6e482ba021d9e17e8127
charlesancheta <cancheta@ualberta.ca>
2020-04-16 06:07:12 -0600

    finish author routes

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
   name: {
@@ -7,4 +8,15 @@ const authorSchema = new mongoose.Schema({
   }
 })
 
+authorSchema.pre('remove', function(next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err)
+    } else if (books.length > 0) {
+      next(new Error('This author still has books'))
+    } else {
+      next()
+    }
+  })
+})
 module.exports = mongoose.model('Author', authorSchema)
